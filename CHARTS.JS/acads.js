9ab1a8be682f8ffd2e435b587f74d398f1b933ac
codeMaster1509 <165783698+codeMaster1509@users.noreply.github.com>
2024-08-08 10:47:47 +0530

    Update acads.js

--- CHARTS.JS/acads.js
+++ CHARTS.JS/acads.js
@@ -304,6 +304,11 @@ document.addEventListener('DOMContentLoaded', function() {
                     label: 'CPI > 9',
                     backgroundColor: '#E0E3E6',
                     data: [3, 19, 23, 18, 6]
+                },
+                {
+                    label: 'CPI not choosen',
+                    backgroundColor: '#1E4460',
+                    data: [0,1,1,0,0]
                 }
             ]
         },
